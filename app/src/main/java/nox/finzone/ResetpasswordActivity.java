44abfb95292b48a513f78c9100a6a6e0487918dc
Arpitaben Patel <Apatel9273@conestogac.on.ca>
2017-04-17 18:47:44 -0400

    FinaZone Master

---
+++ app/src/main/java/nox/finzone/ResetpasswordActivity.java
@@ -0,0 +1,13 @@
+package nox.finzone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ResetpasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resetpassword);
+    }
+}
